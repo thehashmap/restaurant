5538ba4f334cc16dc86c730e5121a01f01e711e7
Nikhil <nkmnikhil0009@gmail.com>
2021-10-13 20:02:39 +0530

    update

--- js/script.js
+++ js/script.js
@@ -12,4 +12,14 @@ document.addEventListener('click' , function(e) {
     if(e.target.closest('.nav-item')) {
         toggleNav();
     }
-})
+});
+
+// sticky header
+window.addEventListener("scroll" , function() {
+    if(this.pageYOffset > 60) {
+        document.querySelector(".header").classList.add("sticky");
+    }
+    else {
+        document.querySelector(".header").classList.remove("sticky");
+    }
+});
